594ac754ba60a1596f5b1e46399fa2a4af8ca6bd
djmartin2019 <djmartin2019@gmail.com>
2025-04-24 14:16:10 -0500

    updated package.json with contentlayer build and next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-// next.config.mjs  (ES-module)
 import { withContentlayer } from "next-contentlayer";
 import nextMdx from "@next/mdx";
 import rehypePrism from "rehype-prism-plus";
+import path from "path";
 
 const withMDX = nextMdx({
   extension: /\.mdx?$/,
@@ -14,6 +14,10 @@ const withMDX = nextMdx({
 export default withContentlayer(
   withMDX({
     pageExtensions: ["ts", "tsx", "md", "mdx"],
+    webpack(config) {
+      config.resolve.alias["@"] = path.resolve(__dirname, "src");
+      return config;
+    },
   })
 );
 
