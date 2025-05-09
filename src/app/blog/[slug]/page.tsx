3ee8fd3bed6b1b50f9f7e646f09628869b012ef1
djmartin2019 <djmartin2019@gmail.com>
2025-05-08 19:13:15 -0500

    Fixed twitter card

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -32,7 +32,7 @@ export async function generateMetadata({
             type: "article",
         },
         twitter: {
-            card: "summary",
+            card: "summary_large_image",
             title: post.title,
             description: post.summary,
             images: [FAVICON],
